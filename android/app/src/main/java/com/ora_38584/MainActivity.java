311f7f84c3cb0ea0bfa7e61d0612d24b69c03d08
Crowdbotics <team@crowdbotics.com>
2023-01-31 00:52:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ora_38584/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ora_38584;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ora_38584";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ora-38584",
+  url: "https://ora-38584.botics.co",
+  api: "https://ora-38584.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
